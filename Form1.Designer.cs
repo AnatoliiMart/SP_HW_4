457f371c3b7442b173b5710e568621fa7823b2bd
Anatolii Martyniuk <Mart_kr93@student.itstep.org>
2023-09-01 15:03:15 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace SP_HW_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pB_5 = new System.Windows.Forms.ProgressBar();
+            this.pB_4 = new System.Windows.Forms.ProgressBar();
+            this.pB_3 = new System.Windows.Forms.ProgressBar();
+            this.pB_2 = new System.Windows.Forms.ProgressBar();
+            this.pB_1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pB_10 = new System.Windows.Forms.ProgressBar();
+            this.pB_9 = new System.Windows.Forms.ProgressBar();
+            this.pB_8 = new System.Windows.Forms.ProgressBar();
+            this.pB_7 = new System.Windows.Forms.ProgressBar();
+            this.pB_6 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.pB_5);
+            this.groupBox1.Controls.Add(this.pB_4);
+            this.groupBox1.Controls.Add(this.pB_3);
+            this.groupBox1.Controls.Add(this.pB_2);
+            this.groupBox1.Controls.Add(this.pB_1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(384, 425);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(7, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(370, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Sidebar Dance";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pB_5
+            // 
+            this.pB_5.Location = new System.Drawing.Point(6, 164);
+            this.pB_5.Name = "pB_5";
+            this.pB_5.Size = new System.Drawing.Size(371, 30);
+            this.pB_5.TabIndex = 4;
+            // 
+            // pB_4
+            // 
+            this.pB_4.Location = new System.Drawing.Point(6, 128);
+            this.pB_4.Name = "pB_4";
+            this.pB_4.Size = new System.Drawing.Size(371, 30);
+            this.pB_4.TabIndex = 3;
+            // 
+            // pB_3
+            // 
+            this.pB_3.Location = new System.Drawing.Point(6, 92);
+            this.pB_3.Name = "pB_3";
+            this.pB_3.Size = new System.Drawing.Size(371, 30);
+            this.pB_3.TabIndex = 2;
+            // 
+            // pB_2
+            // 
+            this.pB_2.Location = new System.Drawing.Point(7, 56);
+            this.pB_2.Name = "pB_2";
+            this.pB_2.Size = new System.Drawing.Size(371, 30);
+            this.pB_2.TabIndex = 1;
+            // 
+            // pB_1
+            // 
+            this.pB_1.Location = new System.Drawing.Point(7, 20);
+            this.pB_1.Name = "pB_1";
+            this.pB_1.Size = new System.Drawing.Size(371, 30);
+            this.pB_1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.pB_10);
+            this.groupBox2.Controls.Add(this.pB_9);
+            this.groupBox2.Controls.Add(this.pB_8);
+            this.groupBox2.Controls.Add(this.pB_7);
+            this.groupBox2.Controls.Add(this.pB_6);
+            this.groupBox2.Location = new System.Drawing.Point(403, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(537, 425);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(15, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 30);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Horse 5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(15, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 30);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Horse 4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(15, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 30);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Horse 3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(15, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 30);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Horse 2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 30);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Horse 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(259, 199);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(271, 213);
+            this.listBox1.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(8, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 46);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Run Horse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pB_10
+            // 
+            this.pB_10.Location = new System.Drawing.Point(131, 164);
+            this.pB_10.Name = "pB_10";
+            this.pB_10.Size = new System.Drawing.Size(399, 30);
+            this.pB_10.TabIndex = 10;
+            // 
+            // pB_9
+            // 
+            this.pB_9.Location = new System.Drawing.Point(131, 128);
+            this.pB_9.Name = "pB_9";
+            this.pB_9.Size = new System.Drawing.Size(399, 30);
+            this.pB_9.TabIndex = 9;
+            // 
+            // pB_8
+            // 
+            this.pB_8.Location = new System.Drawing.Point(131, 92);
+            this.pB_8.Name = "pB_8";
+            this.pB_8.Size = new System.Drawing.Size(399, 30);
+            this.pB_8.TabIndex = 8;
+            // 
+            // pB_7
+            // 
+            this.pB_7.Location = new System.Drawing.Point(132, 56);
+            this.pB_7.Name = "pB_7";
+            this.pB_7.Size = new System.Drawing.Size(399, 30);
+            this.pB_7.TabIndex = 7;
+            // 
+            // pB_6
+            // 
+            this.pB_6.Location = new System.Drawing.Point(132, 20);
+            this.pB_6.Name = "pB_6";
+            this.pB_6.Size = new System.Drawing.Size(399, 30);
+            this.pB_6.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar pB_5;
+        private System.Windows.Forms.ProgressBar pB_4;
+        private System.Windows.Forms.ProgressBar pB_3;
+        private System.Windows.Forms.ProgressBar pB_2;
+        private System.Windows.Forms.ProgressBar pB_1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar pB_10;
+        private System.Windows.Forms.ProgressBar pB_9;
+        private System.Windows.Forms.ProgressBar pB_8;
+        private System.Windows.Forms.ProgressBar pB_7;
+        private System.Windows.Forms.ProgressBar pB_6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SP_HW_4
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<ProgressBar> _dancers;
+
+        private readonly List<ProgressBar> _horses;
+
+        private readonly List<Thread> _threads = new List<Thread>();
+
+        private readonly SynchronizationContext _context;
+
+        private Thread _thread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _context = SynchronizationContext.Current;
+            _dancers = new List<ProgressBar>() { pB_1, pB_2, pB_3, pB_4, pB_5 };
+            _horses = new List<ProgressBar>() { pB_6, pB_7, pB_8, pB_9, pB_10 };
+            foreach (var item in _horses)
+            {
+                item.Value = 0;
+                item.Maximum = 1000;
+                item.Minimum = 0;
+            }
+        }
+
+        private void DancePb()
+        {
+            while (true)
+            {
+                foreach (var item in _dancers)
+                {
+                    _context.Send(d => item.Minimum = 1, null);
+                    _context.Send(d => item.Maximum = 250, null);
+                    _context.Send(d => item.Value = new Random().Next(item.Minimum, item.Maximum), null);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        private void HorseRacing()
+        {
+            int currentThreadIndex = int.Parse(Thread.CurrentThread.Name.Substring(Thread.CurrentThread.Name.Length - 1).ToString());
+
+            while (_horses.ElementAt(currentThreadIndex).Value != _horses.ElementAt(currentThreadIndex).Maximum)
+            {
+                int currentDistance = _horses.ElementAt(currentThreadIndex).Value;
+                int takeDistance = new Random().Next(0, 70);
+
+                if (currentDistance + takeDistance > _horses.ElementAt(currentThreadIndex).Maximum)
+                {
+                    _context.Send(d => _horses.ElementAt(currentThreadIndex).Value +=
+                                      _horses.ElementAt(currentThreadIndex).Maximum - currentDistance, null);
+                }
+                else if (currentDistance + takeDistance <= _horses.ElementAt(currentThreadIndex).Maximum)
+                {
+                    _context.Send(d => _horses.ElementAt(currentThreadIndex).Value += takeDistance, null);
+                }
+
+                Thread.Sleep(250);
+            }
+
+            if (_horses.ElementAt(currentThreadIndex).Value == _horses.ElementAt(currentThreadIndex).Maximum)
+                _context.Send(d => listBox1.Items.Add($"Horse number: {currentThreadIndex + 1}"), null);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < _horses.Count; i++)
+            {
+                Thread thread = new Thread(HorseRacing);
+                thread.Name = $"thread{i}";
+                _threads.Add(thread);
+            }
+
+            foreach (var item in _threads)
+            {
+                item.Start();
+                Thread.Sleep(50);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _thread = new Thread(new ThreadStart(DancePb));
+            _thread.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_thread != null)
+                _thread.Abort();
+
+            foreach (var item in _threads)
+                if (item != null)
+                    item.Abort();
+        }
+
+
+    }
+}
